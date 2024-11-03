2ba26a5cceeb539fdafddcd0d7afe9ea0a2869c9
biruk fekadu <birukefekadu632@gmail.com>
2024-11-03 18:11:46 +0300

    Update Resume page with icon components and info | 50 character limit
    (The commit message summarizes that the Resume page has been updated with the addition of icon components and an "about" info object.)

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,59 @@
-const Resume = () => {
-  return (
-    <div>resume page</div>
-  )
+"use client";
+
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+const about = {
+  title: "About me",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. necessitatibus laborum dicta ullam ut placeat!",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Biruk Fekadu"
+    },
+
+    {
+      fieldName: "Phone",
+      fieldValue: "(+251) 938 741 514"
+    },
+
+    {
+      fieldName: "Experience",
+      fieldValue: "1+ Years"
+    },
+
+    {
+      fieldName: "Email",
+      fieldValue: "birukefekadu632@gmail.com"
+    },
+
+    {
+      fieldName: "Freelance",
+      fieldValue: "Biruk Fekadu"
+    },
+
+    {
+      fieldName: "LinkedIn",
+      fieldValue: "birukfekadu",
+    },
+
+    {
+      fieldName:"Languages",
+      fieldValue:"English, Afan-Oromo, Amharic"
+    },
+
+    
+  ]  
+};
+
+//experience date
+const experience = {
+  icon: '/assets/resume/badge.svg'
 }
 
-export default Resume
+const Resume = () => {
+  return <div>resume page</div>;
+};
+//1:37:20
+export default Resume;
