8b41a67a57e9ef9591552d3bcd11e8a7390998ee
biruk fekadu <birukefekadu632@gmail.com>
2024-11-04 15:34:05 +0300

    Update resume: add experience, education sections in present tense
    This commit updates the resume page by adding new sections for experience and education. It includes relevant details about the author's professional background and academic achievements.

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -10,27 +10,27 @@ const about = {
   info: [
     {
       fieldName: "Name",
-      fieldValue: "Biruk Fekadu"
+      fieldValue: "Biruk Fekadu",
     },
 
     {
       fieldName: "Phone",
-      fieldValue: "(+251) 938 741 514"
+      fieldValue: "(+251) 938 741 514",
     },
 
     {
       fieldName: "Experience",
-      fieldValue: "1+ Years"
+      fieldValue: "1+ Years",
     },
 
     {
       fieldName: "Email",
-      fieldValue: "birukefekadu632@gmail.com"
+      fieldValue: "birukefekadu632@gmail.com",
     },
 
     {
       fieldName: "Freelance",
-      fieldValue: "Biruk Fekadu"
+      fieldValue: "Biruk Fekadu",
     },
 
     {
@@ -39,21 +39,74 @@ const about = {
     },
 
     {
-      fieldName:"Languages",
-      fieldValue:"English, Afan-Oromo, Amharic"
+      fieldName: "Languages",
+      fieldValue: "English, Afan-Oromo, Amharic",
     },
-
-    
-  ]  
+  ],
 };
 
-//experience date
+//experience data
 const experience = {
-  icon: '/assets/resume/badge.svg'
-}
+  icon: "/assets/",
+  title: "My Experience",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae",
+  item: [
+    {
+      company: "Freelance",
+      position: "Software Engineer",
+      duration: "2024- Present",
+    },
+
+    {
+      company: "Jimma University AI center",
+      position: "Fullstack Engineer Intern",
+      duration: "Summer of 2023",
+    },
+
+    {
+      company: "Wallet software Incubation center",
+      position: "Junior Software Engineer, Remote",
+      duration: "4 months",
+    },
+
+    {
+      company: "Ayyoo Printing PLC",
+      position: "Graphics Designer",
+      duration: "17 Months",
+    },
+  ],
+};
+
+//experience data
+const education = {
+  icon: "/assets/",
+  title: "My Education",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae",
+  item: [
+    {
+      institution: "Jimma University",
+      degree: "Computer Science, BSc",
+      duration: "4.6 Years (55 months)",
+    },
+
+    {
+      institution: "Alison",
+      degree: " Diploma Software Project Management",
+      duration: "2024",
+    },
+
+    {
+      institution: "Udemy",
+      degree: "",
+      duration:"",
+    }
+  ],
+};
 
 const Resume = () => {
   return <div>resume page</div>;
 };
-//1:37:20
+
 export default Resume;
