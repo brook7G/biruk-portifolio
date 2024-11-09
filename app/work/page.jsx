2c1075b425c33e61f6b529d9ae2e3bdc21a1b637
biruk fekadu <birukefekadu632@gmail.com>
2024-11-09 23:07:50 +0300

    "Add project data and images" diff --git a/app/work/page.jsx b/app/work/page.jsx ... new file mode 100644 index 0000000..fac97a7 Binary files /dev/null and a/app/work/page.jsx differ

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -13,10 +13,67 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@radix-ui/react-tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+import { Github } from "lucide-react";
 // 2:13:--
 
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae ut rerum adipisci.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }],
+    image: "/work/thumbnail-1.png",
+    live: "",
+    github: "",
+  },
+
+  {
+    num: "02",
+    category: "Full-stack",
+    title: "project 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae ut rerum adipisci.",
+    stack: [
+      { name: "React.js" },
+      { name: "Node.js" },
+      { name: "Express" },
+      { name: "Mongodb" },
+    ],
+    image: "/work/thumbnail-1.png",
+    live: "",
+    github: "",
+  },
+];
+
 const Work = () => {
-  return <div>work page</div>;
+  const [project, setProject] = useState(projects[0]);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+              {/* outline num*/}
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">slider</div>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Work;
