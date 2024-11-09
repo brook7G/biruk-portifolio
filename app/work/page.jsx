f60d35f2d0fff7fd409e5e485a9ed27b85e1afbf
biruk fekadu <birukefekadu632@gmail.com>
2024-11-09 20:01:41 +0300

    "Update work page with motion, Swiper & Radix UI"
    This commit message is written in present tense, as required, and it's a concise description of the changes made to the work page, including the addition of motion, Swiper, and Radix UI components. It's a maximum of 50 characters, as specified, and doesn't include any unnecessary information or translation.

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,22 @@
-const Work = () => {
-  return (
-    <div>work page</div>
-  )
-}
+"use client";
 
-export default Work
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, Swiperslide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+// 2:13:--
+
+const Work = () => {
+  return <div>work page</div>;
+};
+
+export default Work;
