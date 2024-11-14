607a56693cd8febe39f8f37e3ace5066529bc628
biruk fekadu <birukefekadu632@gmail.com>
2024-11-14 18:40:58 +0300

    "Update work page with new project data and Swiper component"
    The commit message "Update work page with new project data and Swiper component" is a concise and clear summary of the changes made in the code diff. It is written in present tense and is a maximum of 50 characters, as specified. The message conveys that the work page has been updated with new project data and a Swiper component has been added, which are the main modifications in the provided code diff.

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { easeIn, motion } from "framer-motion";
 import React, { useState } from "react";
 
-import { Swiper, Swiperslide } from "swiper/react";
+import { Swiper, SwiperSlide, Swiperslide } from "swiper/react";
 import "swiper/css";
 
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
@@ -17,6 +17,7 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import { Github } from "lucide-react";
+import WorkSliderBtns from "@/components/WorkSliderBtns";
 // 2:13:--
 
 const projects = [
@@ -27,7 +28,7 @@ const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae ut rerum adipisci.",
     stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }],
-    image: "/work/thumbnail-1.png",
+    image: "/thumbnail-GOJO.png",
     live: "",
     github: "",
   },
@@ -44,7 +45,36 @@ const projects = [
       { name: "Express" },
       { name: "Mongodb" },
     ],
-    image: "/work/thumbnail-1.png",
+    image: "/thumbnail-JU.png",
+    live: "",
+    github: "",
+  },
+
+  {
+    num: "03",
+    category: "Frontend",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae ut rerum adipisci.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }],
+    image: "/thumbnail-GOJO.png",
+    live: "",
+    github: "",
+  },
+
+  {
+    num: "04",
+    category: "Full-stack",
+    title: "project 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae ut rerum adipisci.",
+    stack: [
+      { name: "React.js" },
+      { name: "Node.js" },
+      { name: "Express" },
+      { name: "Mongodb" },
+    ],
+    image: "/thumbnail-JU.png",
     live: "",
     github: "",
   },
@@ -53,10 +83,18 @@ const projects = [
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
 
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
@@ -125,7 +163,39 @@ const Work = () => {
               </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%]">slider</div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full ">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      {/* overlay */}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      {/* image */}
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-cover"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              {/* slider butttons */}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none "
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all "
+              />
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
