628804c9030d6d64e0dce4df008c1f2f30bbb04f
biruk fekadu <birukefekadu632@gmail.com>
2024-11-14 22:20:23 +0300

    "Add contact form & info" diff --git a/app/contact/page.jsx b/app/contact/page.jsx ...

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,89 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+// import { icons } from "lucide-react";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "+251 938 74 1514",
+  },
+
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "birukefekadu632@gmail.com",
+  },
+
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "Variety, Jimma, Ethiopia ",
+  },
+
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "+251 938 74 1514",
+  },
+];
+
+import { motion } from "framer-motion";
+
 const Contact = () => {
   return (
-    <div>contact page</div>
-  )
-}
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Let's Hustle Together</h3>
+              <p className="text-white/60">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Voluptatibus repellat autem sequi ipsa magni in dolore vitae.
+              </p>
 
-export default Contact
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="Email Address" />
+                <Input type="phone" placeholder="Phone Number" />
+              </div>
+            </form>
+          </div>
+
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            info
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Contact;
