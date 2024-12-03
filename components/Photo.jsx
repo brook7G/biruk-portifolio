3deedbd8a67f1b19845b45e41c39f287b66ca9c5
biruk fekadu <birukefekadu632@gmail.com>
2024-12-03 15:42:19 +0300

    update

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -22,7 +22,7 @@ const Photo = () => {
           }}
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
-          <Image 
+          <Image
             src="/biruk2.png"
             priority
             quality={100}
@@ -47,15 +47,15 @@ const Photo = () => {
             strokeWidth="4"
             strokeLinecap="round"
             strokeLinejoin="round"
-            initial={{strokeDasharray: "24 10 0 0"}}
+            initial={{ strokeDasharray: "24 10 0 0" }}
             animate={{
               strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-              rotate:[120, 360],
+              rotate: [120, 360],
             }}
             transition={{
               duration: 20,
               repeat: Infinity,
-              repeatType: "reverse"
+              repeatType: "reverse",
             }}
           />
         </motion.svg>
